cbef30cf70e67b6a46588bdd1e27fd771fe47b1e
usheanna <yuan.yusiyana@gmail.com>
2025-06-16 16:10:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const menu = document.querySelector('.menu');
 // membuat event click
 // pada saat tombol di click, tambahkan class aktif pada class menu
 // saat diklik lagi, maka class aktif dihilangkan dari class menu (toggle)
-tombol.addEventListener('click', () => {
+//tombol.addEventListener('click', () => {
     menu.classList.toggle('aktif');
 });
 
